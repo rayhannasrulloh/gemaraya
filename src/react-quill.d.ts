0f446ce3ee03495754ecee9850169a6efd4b3bb2
Rayhan Nasrulloh <raihan.rosidi354n@gmail.com>
2026-03-20 10:01:54 +0700

    fix: add typescript definitions for react-quill-new

--- src/react-quill.d.ts
+++ src/react-quill.d.ts
@@ -1,2 +1,21 @@
 // src/react-quill.d.ts
-declare module 'react-quill-new';
+
+declare module 'react-quill-new' {
+  import React from 'react';
+
+  // Kita mendefinisikan "kontrak" props apa saja yang diterima oleh ReactQuill
+  export interface ReactQuillProps {
+    theme?: string;
+    value?: string;
+    onChange?: (content: string, delta: any, source: string, editor: any) => void;
+    modules?: any;
+    formats?: string[];
+    bounds?: string | HTMLElement;
+    children?: React.ReactElement<any>;
+    className?: string;
+    placeholder?: string;
+    readOnly?: boolean;
+  }
+
+  export default class ReactQuill extends React.Component<ReactQuillProps> {}
+}
