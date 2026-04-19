3553ae9df7f5c82d6f1640e3c3c835f692e8048d
Rayhan Nasrulloh <raihan.rosidi354n@gmail.com>
2026-04-19 18:16:33 +0700

    fix: smoothTouch on SmoothScroll.tsx

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -10,9 +10,7 @@ export default function SmoothScroll() {
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: 'vertical',
       gestureOrientation: 'vertical',
-      smoothWheel: true,
       wheelMultiplier: 1,
-      smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
     });
